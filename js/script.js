74276b622ab6087453ef4e0fc2ab290f64cd7473
Usen <83946606+Usen-ita@users.noreply.github.com>
2022-08-03 17:40:37 +0100

    lesson 11 finishing point

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*const usen = "Usen-ita Usen Asanga ";
+var _usen = "Usen-ita Usen Asanga ";
+var $usen = "Usen-ita Usen Asanga ";
+var usenIta = "Usen-ita Usen Asanga ";
+var UsenIta = "Usen-ita Usen Asanga ";
+let usen = "Usen-ita Usen Asanga ";
+usen = "Usen-it Usen Asanga ";
+const usen = "Usen-ita Usen Asanga ";
+usen = "Usen-ita Usen Asanga ";
+document.write(usen)*/
+
+//javaScript Data types Tutorial
+/*var x = 23
+x="false"
+x=undefined
+x=['html', 'php', 23]
+x={
+    name:"adwan minj",
+    age:329
+}
+document.write("the datatype of *"+ x + "* is a " )
+document.write("<br>")
+document.write(typeof x)*/
+
+//javaScript Arithmetic Operators Tutorial
+/*var x = 2;
+var y = 3;
+var z = x-y;//subtraction
+var z = x*y;//multiplication
+var z = x/y;//division
+var z = x%y;//modulus
+var z = x**y;//exponential/to the power of
+document.write((x+y)*2);
+document.write("<br>");
+x--
+document.write(x+y);*/
+
+//JavaScript Assignment Operators
+/*var x = 10
+var y = 4
+//x=x+y;
+//x-=y
+//x*=y
+//x/=y
+//x%=y
+//x**=y
+document.write(x)*/
+
+//JavaScript with Google Chrome Console Part 1
+/*var x = 34
+document.write(x)
+console.warn("something went wrong")
+console.clear()*/
+
+//
